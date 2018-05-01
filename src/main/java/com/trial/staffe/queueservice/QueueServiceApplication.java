807d80cbed396e32a774419d57449bda6a6e6d99
nqbgithub <nqb13510951690@sina.com>
2018-05-01 11:13:27 +0800

    up

---
+++ src/main/java/com/trial/staffe/queueservice/QueueServiceApplication.java
@@ -0,0 +1,14 @@
+package com.trial.staffe.queueservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class QueueServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueueServiceApplication.class, args);
+    }
+}
---
+++ src/test/java/com/trial/staffe/queueservice/QueueServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.trial.staffe.queueservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QueueServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
